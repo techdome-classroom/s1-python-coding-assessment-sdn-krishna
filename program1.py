3fd232b3d2eadbad1db8f8851d73cd5c3a4f820a
sdn-krishna <singasanidurganitishkrishna@gmail.com>
2024-11-11 13:53:19 +0530

    Initial commit for coding assessment

--- program1.py
+++ program1.py
@@ -1,6 +1,24 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        if not grid or not grid[0]:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        island_count = 0
+
+        def dfs(i, j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == 'W':
+                return
+            grid[i][j] = 'W'
+            dfs(i + 1, j) 
+            dfs(i - 1, j) 
+            dfs(i, j + 1) 
+            dfs(i, j - 1)  
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 'L':
+                    island_count += 1
+                    dfs(i, j)  
+
+        return island_count
--- program2.py
+++ program2.py
@@ -1,5 +1,19 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    m, n = len(s), len(p)
 
-# write your code here
-  
-        return False
+    dp = [[False] * (n + 1) for _ in range(m + 1)]
+
+    dp[0][0] = True
+    
+    for j in range(1, n + 1):
+        if p[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+            elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+    
+    return dp[m][n]
